0239f6987825f2e8aa979c7156011b682b0975da
dumeng <dumeng81@outlook.com>
2023-07-10 14:23:49 +0800

    add select handler

--- hello-world/src/views.tsx
+++ hello-world/src/views.tsx
@@ -1,5 +1,6 @@
 /** @jsx svg */
 import { svg } from 'sprotty/lib/lib/jsx';
+import classnames from 'classnames'
 import { injectable } from 'inversify';
 import { VNode } from 'snabbdom';
 import { IView, RenderingContext, SNode } from 'sprotty';
@@ -8,14 +9,23 @@ import { TaskNode } from './model';
 @injectable()
 export class TaskNodeView implements IView {
     render(node: Readonly<SNode & TaskNode>, context: RenderingContext): VNode {
+        const selected = node.selected
         return <g>
-            <rect class-sprotty-node={true} class-task={true}
-                class-running={node.isRunning}
-                class-finished={node.isFinished}
+            <rect
+                class={{ 'sprotty-node': true, 'task': true, running: node.isRunning, finished: node.isFinished }}
+                // class-sprotty-node={true} class-task={true}
+                //     class-running={node.isRunning}
+                //     class-finished={node.isFinished}
                 width={node.size.width}
                 height={node.size.height}
             >
             </rect>
+            {selected ? <g >
+                <rect class={{ 'task-handler': true }} width={5} height={5} x={0} y={0}></rect>
+                <rect class={{ 'task-handler': true }} width={5} height={5} x={0} y={node.size.height}></rect>
+                <rect class={{ 'task-handler': true }} width={5} height={5} x={node.size.width} y={0} ></rect>
+                <rect class={{ 'task-handler': true }} width={5} height={5} x={node.size.width} y={node.size.height}></rect>
+            </g> : null}
             <text x={node.size.width / 2} y={node.size.height / 2 + 5}>{node.name}</text>
         </g>;
     }
